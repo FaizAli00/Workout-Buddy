2c972a6645a68f11799ed74d918641f7a8d1acaf
FaizAli00 <davyjones403@gmail.com>
2024-11-07 23:12:13 +0500

    new commit

---
+++ frontend/src/Components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <header>
+      <div className="container">
+        <h1>Workout Buddy</h1>
+      </div>
+    </header>
+  )
+}
+
+export default Navbar;
